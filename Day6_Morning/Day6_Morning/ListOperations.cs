1a06622948c70fa12260dd45b9f79568842e68c6
unknown <451785@PC317425.cts.com>
2015-10-15 14:35:01 +0530

    Day6_Morning
    Stacks Arrays

---
+++ Day6_Morning/Day6_Morning/ListOperations.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Morning
+{
+    class ListOperations
+    {
+        public static void operations()
+        {
+            Mylist list = new Mylist();
+            list.Add("one");
+            list.Add("two");
+            list.Add("three");
+            list.Add("four");
+            list.print();
+            list.Remove("two");
+            Console.WriteLine("After remove");
+            list.print();
+        }
+    }
+    class Mylist : IList
+    {
+        private object[] data = new object[10];
+        int count;
+        public Mylist()
+        {
+            count = 0;
+        }
+        public int Add(object val)
+        {
+            if (count < data.Length)
+            {
+                data[count++] = val;
+                return count - 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public void print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+        }
+
+        // IEnumerable Members
+
+        public IEnumerator GetEnumerator()
+        {
+            // Refer to the IEnumerator documentation for an example of
+            // implementing an enumerator.
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        public bool Contains(object value)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (data[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(object value)
+        {
+            int index = -1;
+            for (int i = 0; i < count; i++)
+            {
+                if (data[i] == value)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+
+        }
+
+        public void Remove(object value)
+        {
+            int index = IndexOf(value);
+            if (index >= 0 && index < count)
+            {
+                for (int i = index; i < count - 1; i++)
+                {
+                    data[i] = data[i + 1];
+                }
+                count--;
+            }
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFixedSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Day6_Morning/Day6_Morning/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Morning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sample sam = new sample();
+            //sam.Execute();
+            ListOperations.operations();
+            Console.ReadKey();
+        }
+        public class sample
+        {
+            public void Execute()
+            {
+                myEnumerable myObj = new myEnumerable();
+                var enumerator = myObj.GetEnumerator();
+                    while(enumerator.MoveNext())
+                        Console.WriteLine(enumerator.Current);
+            }
+        }
+        public class MyEnumerator : IEnumerator
+        {
+            int[] data = new int[] { 1, 2, 3, 4, 5,6 };
+            int currentIndex = -1;
+
+            public object Current
+            {
+                get {
+                       return data[currentIndex];
+                }
+                
+            }
+
+            public bool MoveNext()
+            {
+                currentIndex++;
+                if (currentIndex < data.Length-1)
+                {
+                    while (data[currentIndex] % 2 != 0 && currentIndex < data.Length)
+                    {
+                        currentIndex++;
+                    }
+                    if (currentIndex > data.Length)
+                        return false;
+                    else
+                        return true;
+                }
+                else
+                    return false;
+
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+        public class myEnumerable : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                return new MyEnumerator();
+            }
+        }
+    }
+}
